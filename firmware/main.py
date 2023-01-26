e0d86c822fda30704d6d84f409e7546ac5840d6a
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2023-01-26 12:00:23 +0000

    feat: :wrench: Setup Flask server

--- firmware/main.py
+++ firmware/main.py
@@ -1,9 +1,25 @@
+from threading import Thread
+
+from flask import Flask, send_file
+
 from core import load_modules
 
 triggers = load_modules("triggers")
 monitors = load_modules("monitors")
 
 
-while True:
-    for m in monitors:
-        m.on_update(triggers)
+def daemon():
+    while True:
+        for m in monitors:
+            m.on_update(triggers)
+
+
+Thread(target=daemon, daemon=True)
+
+
+app = Flask("sit-down-please")
+
+
+@app.route("/")
+def status():
+    return send_file("data/status.json")
