041e428220b8af984014be85392c27ddb002c4fd
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2023-01-26 09:13:00 +0000

    feat: :sparkles: Add time elapsed card

---
+++ components/SdpTimeElapsedCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card class="mx-auto" min-width="350" min-height="200">
+    <v-card-item title="Elapsed" />
+    <v-card-text class="py-0">
+      <v-row align="center" justify="center" no-gutters>
+        <v-col class="text-h3 text-secondary" cols="8">
+          {{ elapse.getHours().toString().padStart(2, "0") }} :
+          {{ elapse.getMinutes().toString().padStart(2, "0") }}
+        </v-col>
+
+        <v-col class="text-center" cols="4">
+          <v-icon color="secondary" icon="mdi:mdi-timer-sand" size="88" />
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <div class="d-flex py-3 justify-space-around">
+      <v-list-item density="compact" prepend-icon="mdi:mdi-clock-start">
+        {{ new Date(since * 1000).toLocaleTimeString() }}
+      </v-list-item>
+    </div>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { useCurrentTime } from "~~/composition";
+
+const props = defineProps<{
+  since: number;
+}>();
+
+const { currentTime } = useCurrentTime();
+const elapse = computed(
+  () => new Date(currentTime.value.getTime() - props.since * 1000)
+);
+</script>
