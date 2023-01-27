77ef138b4983906d1d3bd542420721d25fb868b9
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2023-01-27 06:14:15 +0000

    feat: :sparkles: Notify when user seat/leave

--- software/pages/index.vue
+++ software/pages/index.vue
@@ -1,8 +1,6 @@
 <template>
   <sdp-clock-card :temp="36" :humid="60" />
-  <sdp-occupied-card
-    :occupied="!!status ? status.seat > status.leave : false"
-  />
+  <sdp-occupied-card :occupied="occupied" />
   <sdp-time-elapsed-card :since="status?.seat ?? Date.now()" />
 </template>
 
@@ -10,7 +8,35 @@
 import SdpClockCard from "~~/components/SdpClockCard.vue";
 import SdpOccupiedCard from "~~/components/SdpOccupiedCard.vue";
 import SdpTimeElapsedCard from "~~/components/SdpTimeElapsedCard.vue";
+
+import logo from "assets/logo.png";
+
 import { useStatus } from "../composition";
 
 const { status } = useStatus();
+
+const occupied = computed(() =>
+  !!status.value ? status.value.seat > status.value.leave : false
+);
+
+onMounted(() => {
+  if (!!Notification && Notification.permission == "default") {
+    Notification.requestPermission();
+  }
+});
+
+watch(occupied, (nowOccupied) =>
+  nowOccupied
+    ? new Notification("Sit Down Please!", {
+        body: "Focus on your work!",
+        icon: logo,
+      })
+    : new Notification("Sit Down Please!", {
+        body: `You has been focused for ${new Date(
+          (Date.now() / 1000 - (status.value?.leave ?? Date.now() / 1000)) *
+            1000
+        ).toLocaleTimeString()}`,
+        icon: logo,
+      })
+);
 </script>
